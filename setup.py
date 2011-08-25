ac07016943e9fe7e6b78d74721910b7f348aa823
tamiel <michael.ricordeau@gmail.com>
2011-08-25 14:44:17 +0200

    0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (2, 6):
 setup(
     name = "plivohelper",
     py_modules = ['plivohelper'],
-    version = "0.1.0",
+    version = "0.1.1",
     description = "Plivo API client and RESTXML generator",
     author = "Plivo Team",
     author_email = "hello@plivo.org",
