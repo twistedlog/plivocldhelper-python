c0ff1194b70cff9b562c8427ff6ac5ee4ae5f079
tamiel <michael.ricordeau@gmail.com>
2011-09-06 11:09:01 +0200

    add reload_cache_config

--- plivohelper.py
+++ plivohelper.py
@@ -143,6 +143,13 @@ class REST(object):
         method = 'POST'
         return self.request(path, method, call_params)
 
+    def reload_cache_config(self, call_params):
+        """REST Reload Plivo Cache Config helper
+        """
+        path = '/' + self.api_version + '/ReloadCacheConfig/'
+        method = 'POST'
+        return self.request(path, method, call_params)
+
     def call(self, call_params):
         """REST Call Helper
         """
